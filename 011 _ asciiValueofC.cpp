e3b7192debbc05a62a7cdcca53daf382615c3b67
dhiaka <74065130+dhiaka@users.noreply.github.com>
2021-02-07 15:44:07 +0100

    Create 011 _ asciiValueofC.cpp

---
+++ 011 _ asciiValueofC.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c;
+    
+    cout << "enter a c character :\n" ;
+    cin >> c;
+    cout << " the ASCII value of " << c << " is " << int(c) << "\n";
+    return 0;
+}
