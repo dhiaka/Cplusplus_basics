667bef0f78829f158490a4b00321dc35dc6caaa6
dhiaka <74065130+dhiaka@users.noreply.github.com>
2021-02-07 15:44:53 +0100

    Create 012 _ productofTWOint.cpp

---
+++ 012 _ productofTWOint.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,y,p;
+    
+    cout << "enter x & y :\n" << endl;
+    cin >>x >>y;
+    
+    //product action 
+    p = x * y ;
+    
+    cout << x << " * " << y << " = " << p << endl;
+    
+    return 0;
+}
