0a5cb166a4b13c468298ccd4b7af2be6686384b2
dhiaka <74065130+dhiaka@users.noreply.github.com>
2021-02-07 15:42:34 +0100

    Create 009 _ sizeofVARS.cpp

---
+++ 009 _ sizeofVARS.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " size of int equals to " << sizeof(int) << " bytes" << endl;
+    cout << " size of float equals to " << sizeof(float) << " bytes" << endl;
+    cout << " size of double equlas to " << sizeof(double) << " bytes" << endl;
+    cout << " size of char equals to " << sizeof(char) << " byte" << endl;
+    
+    return 0;
+}
